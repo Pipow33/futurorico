ad9fa1b97c45c5e7f61bd835b92a2c5c0e9bd3d1
Pedro S <110622208+Pipow33@users.noreply.github.com>
2023-12-01 13:21:02 -0300

    Add files via upload

--- futuro rico oficial final/js/script.js
+++ futuro rico oficial final/js/script.js
@@ -1,39 +1,52 @@
 var taxa = 0;
-
 $(document).ready(function() {
     $("#calculate").click(function() {
-        var ci = parseFloat($("#ci").val());
-        var t = parseFloat($("#t").val());
-        var taxap = 1.06;
-        var taxatd = 1.1225;
-        var taxacdi = 1.1215;
-        var taxalca = 1.1094;
+        function formatarMoeda(numero) {
+            return numero.toLocaleString('pt-BR', {
+                style: 'currency',
+                currency: 'BRL',
+                minimumFractionDigits: 2,
+                maximumFractionDigits: 2
+            });
+        }
 
-        var totalp = ci * Math.pow(taxap, t); // Valor inicial × (1,06)^tempo
-        $("#p-totalp").text("Total Acumulado: R$" + totalp.toFixed(2)); 
-        lucrop = totalp - ci;
-        $("#p-lucrop").text("Lucro: R$" + lucrop.toFixed(2));
-        var totaltd = ci * Math.pow(taxatd, t); // Valor inicial × (1,1225)^tempo
-        $("#p-totaltd").text("Total Acumulado: R$"+ totaltd.toFixed(2));
-        var lucrotd = totaltd - ci;
-        $("#p-lucrotd").text("Lucro: R$" + lucrotd.toFixed(2))
-        var totalcdi = ci * Math.pow(taxacdi, t); // Valor inicial × (1,1215)^tempo
-        $("#p-totalcdi").text("Total Acumulado: R$"+ totalcdi.toFixed(2));
-        var lucrocdi = totalcdi - ci;
-        $("#p-lucrocdi").text("Lucro: R$" + lucrocdi.toFixed(2))
-        var totallca = ci * Math.pow(taxalca, t); // Valor inicial × (1,1094)^tempo
-        $("#p-totallca").text("Total Acumulado: R$"+ totallca.toFixed(2));
-        var lucrolca = totallca - ci;
-        $("#p-lucrolca").text("Lucro: R$" + lucrolca.toFixed(2))
-            $("#resultado-p").click();
-            $("#resultado-td").click();
-            $("#resultado-cdi").click();
-            $("#resultado-lca").click();
-            $('html, body').animate({
-                scrollTop: $(".accordion-button.collapsed").offset().top
-                }, 1000); // Ajuste a velocidade conforme necessário
-        
-        
+    var ci = parseFloat($("#ci").val());
+    var t = parseFloat($("#t").val());
+    var taxap = 1.06;
+    var taxatd = 1.1225;
+    var taxacdi = 1.1215;
+    var taxalca = 1.1094;
 
-    });
+    var totalp = ci * Math.pow(taxap, t);
+    $("#p-totalp").text("Total Acumulado: " + formatarMoeda(totalp));
+
+    var lucrop = totalp - ci;
+    $("#p-lucrop").text("Lucro: " + formatarMoeda(lucrop));
+
+    var totaltd = ci * Math.pow(taxatd, t);
+    $("#p-totaltd").text("Total Acumulado: " + formatarMoeda(totaltd));
+
+    var lucrotd = totaltd - ci;
+    $("#p-lucrotd").text("Lucro: " + formatarMoeda(lucrotd));
+
+    var totalcdi = ci * Math.pow(taxacdi, t);
+    $("#p-totalcdi").text("Total Acumulado: " + formatarMoeda(totalcdi));
+
+    var lucrocdi = totalcdi - ci;
+    $("#p-lucrocdi").text("Lucro: " + formatarMoeda(lucrocdi));
+
+    var totallca = ci * Math.pow(taxalca, t);
+    $("#p-totallca").text("Total Acumulado: " + formatarMoeda(totallca));
+
+    var lucrolca = totallca - ci;
+    $("#p-lucrolca").text("Lucro: " + formatarMoeda(lucrolca));
+
+    $("#resultado-p").toggleClass("accordion-collapse");
+    $("#resultado-td").toggleClass("accordion-collapse");
+    $("#resultado-cdi").toggleClass("accordion-collapse");
+    $("#resultado-lca").toggleClass("accordion-collapse");
+    $('html, resultado').animate({
+            scrollTop: $("#resultado").offset().top
+        }, 1000);
+    });     
 });
